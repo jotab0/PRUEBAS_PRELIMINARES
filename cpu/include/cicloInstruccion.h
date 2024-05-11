7901c3ed2c1c3221e5ac5546eabd7f517b9217ef
roylischi <rlischinsky@frba.utn.edu.ar>
2024-05-11 20:50:29 +0000

    recibir pcb

--- cpu/include/cicloInstruccion.h
+++ cpu/include/cicloInstruccion.h
@@ -1,3 +1,18 @@
+#ifndef CICLOINSTRUCCION_H_
+#define CICLOINSTRUCCION_H_ 
+
 #include "c_gestor.h"
 
+t_log* cpu_logger;
+t_log* cpu_log_obligatorio;
 
+char** opcode_headers;
+
+char** instruccion_elements;
+
+//Semaforos
+sem_t sem_control_fetch_decode;
+sem_t sem_control_decode_execute;
+sem_t sem_control_execute_checkinterruption; //hace falta este?
+
+#endif
--- cpu/include/comunicaciones_cpu.h
+++ cpu/include/comunicaciones_cpu.h
@@ -7,4 +7,22 @@ void esperar_kernel_cpu_dispatch();
 void esperar_kernel_cpu_interrupt();
 void esperar_memoria_cpu();
 
+pthread_mutex_t mutex_manejo_contexto;
+
+typedef struct{
+    int proceso_pid;
+    int proceso_pc;
+    int proceso_tiempo_ejecutado;
+    int proceso_ticket;
+    uint32_t AX;
+    uint32_t BX; 
+    uint32_t CX;
+    uint32_t DX;
+}t_contexto;
+
+t_contexto* contexto;
+
+t_log* cpu_logger;
+t_log* cpu_log_obligatorio;
+
 #endif
